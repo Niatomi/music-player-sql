8e0fa7935cae0dc4c6e794db295bb4b14fb664c0
Niatomi <playervoker@gmail.com>
2022-04-07 10:16:29 +0400

    Changed folder to model and changed return type variable

--- music-player-sql-maven/src/main/java/ru/niatomi/models/Playable.java
+++ music-player-sql-maven/src/main/java/ru/niatomi/models/Playable.java
@@ -1,14 +1,13 @@
-/*
+package ru.niatomi.models;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package musicplayersql.Classes.Substanses;
 
 /**
  *
  * @author Niatomi
  */
 public interface Playable {
-    boolean play();
+    void play();
 }
