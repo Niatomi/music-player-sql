c9efeb597526632c6703f8fed2ff616401e3247d
Niatomi <playervoker@gmail.com>
2022-04-07 10:21:05 +0400

    Changed package location

--- music-player-sql-maven/src/main/java/ru/niatomi/view/Viewable.java
+++ music-player-sql-maven/src/main/java/ru/niatomi/view/Viewable.java
@@ -1,11 +1,8 @@
-/*
+package ru.niatomi.view;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package musicplayersql.Classes.VIEW;
-
-import musicplayersql.Classes.Generics.ArtistDAO;
 
 /**
  *
