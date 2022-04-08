8b8af02522ae8a2ea5a213db45844959d9c82af4
Niatomi <playervoker@gmail.com>
2022-04-08 23:52:08 +0400

    Add dataSource for JDBCTemplate

--- music-player-sql-maven/src/main/java/ru/niatomi/config/SpringConfig.java
+++ music-player-sql-maven/src/main/java/ru/niatomi/config/SpringConfig.java
@@ -2,7 +2,12 @@ package ru.niatomi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @author niatomi
@@ -16,6 +21,23 @@ public class SpringConfig {
     @Autowired
     public SpringConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/music_player_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("admin");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 
 }
